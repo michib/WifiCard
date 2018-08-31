73524470fc0c92d231765cde82aadc3f92df820a
Michael <M.Flower@gmx.de>
2018-08-31 23:49:38 +0200

    First shot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'WifiImage';
+
+  wifi$: Observable<string> = of('test');
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    const initial = {
+      ssid: 'Router',
+      password: 'Password123'
+    };
+    this.form = this.formBuilder.group(initial);
+
+    this.wifi$ = this.form.valueChanges.pipe(
+      startWith(initial),
+      map(this.mapToWifi)
+    );
+  }
+
+  private mapToWifi(value: { ssid: string, password: string}): string {
+    return `WIFI:T:WPA;S:${value.ssid};P:${value.password};;`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatIconRegistry, MatInputModule } from '@angular/material';
+import { QRCodeModule } from 'angularx-qrcode';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    QRCodeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    MatIconRegistry
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public matIconRegistry: MatIconRegistry) {
+    matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+  }
+}
